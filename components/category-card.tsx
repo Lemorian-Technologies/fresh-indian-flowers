237059813d0b7d1e9af41b0e4af1c9fe849fbba5
Arun Shankar Ezhilarasan <arun.ezhilarasan@betmakers.com>
2024-09-18 17:17:19 +0530

    moved buy button to bottom

--- components/category-card.tsx
+++ components/category-card.tsx
@@ -10,13 +10,13 @@ interface CardProps {
 export const Card: React.FC<CardProps> = ({ title, description, link, cardImage }) => {
   return (
     <article className="duration-400 transform overflow-hidden rounded-lg bg-white shadow-none transition-transform ease-in-out hover:scale-105 hover:text-custom-green hover:shadow-lg">
-      <div className="grid grid-cols-1 gap-4">
+      <div className="flex h-full flex-col">
         <figure className="aspect-w-16 aspect-h-9 relative h-72">
           <Image src={cardImage} alt="" fill className="h-auto w-full object-cover" />
         </figure>
-        <div className="flex flex-col p-6">
+        <div className="flex flex-grow flex-col p-6">
           <h2 className="mb-4 text-2xl font-bold transition-colors duration-300">{title}</h2>
-          <p className="mb-4 text-base leading-6 text-black">{description}</p>
+          <p className="mb-4 flex-grow text-base leading-6 text-black">{description}</p>
           <a
             href={link}
             className="inline-flex items-center self-center rounded-full bg-custom-green px-4 py-2 text-white no-underline hover:text-lg focus:outline-none"
