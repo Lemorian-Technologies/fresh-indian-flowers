edab74d40456e5ca958e2ace3e438fee6e37dffc
Arun Shankar Ezhilarasan <arun.ezhilarasan@betmakers.com>
2024-09-24 05:10:36 +0530

    add base url

--- app/reports/weekly/page.jsx
+++ app/reports/weekly/page.jsx
@@ -34,9 +34,11 @@ const DateRangePicker = () => {
     const formattedEndDate = endOfDay.format('');
 
     try {
+      const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000'; // Use 'NEXT_PUBLIC_' for exposing to the client side in Next.js
       const response = await fetch(
-        `http://localhost:3000/api/reports?start=${formattedStartDate}&end=${formattedEndDate}`
+        `${baseUrl}/api/reports?start=${formattedStartDate}&end=${formattedEndDate}`
       );
+
       if (response.ok) {
         const fileBlob = await response.blob();
         fileDownload(fileBlob, 'filename.csv');
@@ -44,6 +46,7 @@ const DateRangePicker = () => {
         console.error('Failed to fetch data');
       }
     } catch (e) {
+      ('');
       console.log('download-exception,' + e);
     }
   }
